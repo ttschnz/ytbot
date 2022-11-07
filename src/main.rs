d39fe39017bf96c04a0440626069b0ff402c5f16
tim <tim@mw3.ch>
2022-11-07 17:43:06 +0100

    add video download to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use ytbot::download_video;
 use ytbot::get_list;
 
 #[tokio::main]
 async fn main() {
     let query = "#fyp";
     let list = get_list(query).await;
-    for video in list {
+    for video in &list {
         println!("{}", video);
     }
+
+    let video = &list[0];
+    let path = "test.mp4";
+    download_video(&video.url, path).await;
+    println!("done downloading video: ./{}", path);
 }
