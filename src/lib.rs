71447e47d6ab161bea0995220c6132ff69882d3e
tim <tim@mw3.ch>
2022-11-07 18:02:13 +0100

    add rest for config

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,13 @@ pub use self::crawler::get_list;
 pub use self::video_info::VideoInfo;
 use std::env;
 
-pub fn get_config() -> (String, String) {
+pub struct Config {
+    pub query: String,
+    pub path: String,
+}
+
+pub fn get_config() -> Config {
     let query = env::var("SEARCH_QUERY").unwrap_or_else(|_| "#trending".into());
     let path = env::var("OUT_PATH").unwrap_or_else(|_| "test.mp4".into());
-    return (query, path);
+    return Config { query, path };
 }
