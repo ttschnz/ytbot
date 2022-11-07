625aa7f42dc6d553c288db76c9ace444c260cfa2
tim <tim@mw3.ch>
2022-11-07 17:55:29 +0100

    read config from env or default

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@ mod video_info;
 pub use self::crawler::download_video;
 pub use self::crawler::get_list;
 pub use self::video_info::VideoInfo;
+use std::env;
+
+pub fn get_config() -> (String, String) {
+    let query = env::var("SEARCH_QUERY").unwrap_or_else(|_| "#trending".into());
+    let path = env::var("OUT_PATH").unwrap_or_else(|_| "test.mp4".into());
+    return (query, path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use ytbot::download_video;
+use ytbot::get_config;
 use ytbot::get_list;
 
 #[tokio::main]
 async fn main() {
-    let query = "#fyp";
-    let list = get_list(query).await;
+    let (query, path) = get_config();
+
+    let list = get_list(&query).await;
     for video in &list {
         println!("{}", video);
     }
 
     let video = &list[0];
-    let path = "test.mp4";
-    download_video(&video.url, path).await;
+    download_video(&video.url, &path).await;
     println!("done downloading video: ./{}", path);
 }
